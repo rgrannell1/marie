0b38dcd4c928e64a121b788e5f4d500b5349d875
Róisín Grannell <r.grannell2@gmail.com>
2022-05-31 19:40:36 +0100

    added tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,13 +1,63 @@
 
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import {Marie} from "../src/marie.ts";
+import { Triple } from "../src/triple.ts";
 
+type TripleTestCase = {
+  document: string,
+  triples: string[][]
+}
 
-const cases = []
+const cases: TripleTestCase[] = []
 cases.push({
+  document: `
+  :a :b
+  `,
+  triples: []
+})
 
+cases.push({
+  document: `
+    :a b c     :b-c-d    :e
+  `,
+  triples: [
+   ['a b c', 'b-c-d', 'e']
+  ]
 })
 
 
-Deno.test("url test", () => {
+const runTripleTest = async (tcase: TripleTestCase) => {
+  const md = new Marie({
+    namespace: ''
+  })
 
-});
+  const triples: string[][] = [];
+
+  for await (const triple of md.parse(tcase.document)) {
+    triples.push([triple.subject, triple.predicate, triple.object])
+  }
+
+  if (triples.length !== tcase.triples.length) {
+    throw new Error(`expected ${tcase.triples.length} triples, received ${triples.length}. For test-case:\n${tcase.document}`)
+  }
+
+  for (let idx = 0; idx < triples.length; ++idx) {
+    let actual = triples[idx]
+    let expected = tcase.triples[idx]
+
+    if (actual.length !== 3) {
+      throw new Error(`expected three elements in triple ${actual}`)
+    }
+
+    assertEquals(actual[0], expected[0])
+    assertEquals(actual[1], expected[1])
+    assertEquals(actual[2], expected[2])
+  }
+}
+
+
+for (const tcase of cases) {
+  Deno.test("Triple Extraction Test", async () => {
+    await runTripleTest(tcase)
+  });
+}
